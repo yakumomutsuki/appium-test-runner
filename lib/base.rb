81988006703323f6cff9ec853090aee94d500b15
AD\munetaka.oi <munetaka.oi@n-currency.com>
2018-06-29 14:08:07 +0900

    root directoryを取得するメソッドを追加し、不要な行を削除

--- lib/base.rb
+++ lib/base.rb
@@ -9,16 +9,18 @@ require './custom_logger'
 #   Implements of Log, check on argument.class Base
 #   When argument is wrong then raise error
 class Base
-
   include Resources::Exceptions
 
   attr_accessor :log
 
   def initialize(os_name:)
     argument_name_check(os_name)
+    log_path = "#{root_dir}/test/logs/#{Time.now.strftime('%Y%m%d')}.log"
+    @log = CustomLogger.new(log_path)
+  end
 
-    @dir = __FILE__.match(%r{.*(appium-test-runner)}).to_s # can get => ".~/appium/"
-    @log = CustomLogger.new("#{@dir}/test/logs/#{Time.now.strftime('%Y%m%d')}.log")
+  # Get root => .~/appium-test-runner/
+  def root_dir
+    __FILE__.match(%r{.*(appium-test-runner)}).to_s
   end
 end
-
