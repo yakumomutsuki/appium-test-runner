70a88734276742bfcdbc64da09f49f2cf30f791c
AD\munetaka.oi <munetaka.oi@n-currency.com>
2018-06-29 12:14:24 +0900

    CustomLoggerクラスにオーバーライドしたcloseメソッドを追加。コメントも追加。

--- lib/custom_logger.rb
+++ lib/custom_logger.rb
@@ -1,15 +1,28 @@
 require 'logger'
 
+# CustomLogger Class is a Extends of Logger Class.
+#
+# Under the methods are Added.
+#  :initialize
+#  :backtrace
+#  :close
 class CustomLogger < Logger
-
+  # Call the constructor and outputs 'start' to log.
   def initialize(logdev)
     super(logdev)
     add(INFO, 'start', nil)
   end
 
+  # Receives error object and outputs backtrace to log.
   def backtrace(error)
     add(ERROR, error.class)
     add(ERROR, error.message)
     add(ERROR, error.backtrace)
+  end
+
+  # Call the overridden method and  and outputs 'end' to log
+  def close
+    add(INFO, 'end', nil)
+    super
   end
 end
